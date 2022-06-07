f969413dc720724f4abff4ba9aa36dadd4181caf
raffaluciano <65427252+raffaluciano@users.noreply.github.com>
2022-06-07 20:40:40 -0300

    Lo que hicimos hoy

--- Problema_2_tp2_variables.m
+++ Problema_2_tp2_variables.m
@@ -4,3 +4,22 @@ L=0.01;
 M=0.2;
 b=0.5;
 k=2;
+
+yeq = 0.2551;
+ieq = 0.5;
+Feq =  (k * ieq^2)/yeq;
+
+ki= (2 * ieq * k)/yeq;
+ky= -(k * ieq^2)/yeq^2;
+
+hold on
+plot(y,'r', 'LineSmoothing','on');
+plot(ylineal, 'LineSmoothing','on');
+xlabel('Tiempo [s]');
+ylabel('Altura [m]');
+%axis([0 10 0 0.6])
+
+hold off
+
+grid;
+legend('y','y incremental');
