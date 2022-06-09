f149b20f6612c5358696b759ef896d82e42aed1c
raffaluciano <lraffagnini21@gmail.com>
2022-06-08 22:56:23 -0300

    Mi primer commit :D

--- Problema_2_tp2_variables.m
+++ Problema_2_tp2_variables.m
@@ -4,22 +4,35 @@ L=0.01;
 M=0.2;
 b=0.5;
 k=2;
+kp=1000;
 
 yeq = 0.2551;
 ieq = 0.5;
 Feq =  (k * ieq^2)/yeq;
+ueq = 10;
 
 ki= (2 * ieq * k)/yeq;
 ky= -(k * ieq^2)/yeq^2;
 
+%{
 hold on
 plot(y,'r', 'LineSmoothing','on');
-plot(delta_y + yeq, 'LineSmoothing','on');
+plot(delta_y, 'LineSmoothing','on');
 xlabel('Tiempo [s]');
 ylabel('Altura [m]');
 %axis([0 10 0 0.6])
+%hold off
+%}
 
-hold off
+subplot(311), plot(delta_y + yeq);
+xlabel('Tiempo [s]');
+ylabel('y');grid;
+subplot(312), plot(delta_i + ieq);
+xlabel('Tiempo [s]');
+ylabel('i');grid;
+subplot(313), plot(delta_u + ueq);
+xlabel('Tiempo [s]');
+ylabel('u');grid;
 
 grid;
-legend('y','y incremental');
+%legend('y incremental');
